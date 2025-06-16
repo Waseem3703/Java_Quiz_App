e52463ea1517aa6cb1185ef7d5b583243cc5dd0c
[Waseem] <[waseemkhan3703@gmail.com]>
2025-06-17 01:55:11 +0500

    Added sound effects

--- src/quiz/application/Quiz.java
+++ src/quiz/application/Quiz.java
@@ -3,29 +3,28 @@ package quiz.application;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import javax.sound.sampled.*;
 
 public class Quiz extends JFrame implements ActionListener {
 
-    String[][] questions   = new String[50][5];  
-    String[][] answers     = new String[50][2];  
-    String[][] userAnswers = new String[10][1];  
-    int[]      selected    = new int[10];        
+    String[][] questions   = new String[50][5];
+    String[][] answers     = new String[50][2];
+    String[][] userAnswers = new String[10][1];
+    int[] selected = new int[10];
 
-    JLabel       qNoLabel, qTextLabel, timerLabel;
+    JLabel qNoLabel, qTextLabel, timerLabel;
     JRadioButton opt1, opt2, opt3, opt4;
-    ButtonGroup  group;
-    JButton      nextBtn, submitBtn;
+    ButtonGroup group;
+    JButton nextBtn, submitBtn;
     javax.swing.Timer swingTimer;
 
-    int timer  = 15;  
-    int count  = 0;   
-    int score  = 0;
-
+    int timer = 15, count = 0, score = 0;
     final String studentName;
     final String regNumber;
+
+    private Clip tickClip;
+    private Clip buzzerClip;
 
     public Quiz(String studentName, String regNumber) {
         this.studentName = studentName;
@@ -34,12 +33,13 @@ public class Quiz extends JFrame implements ActionListener {
         setBounds(50, 0, 1440, 850);
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
-        add(new JLabel(new ImageIcon(
-                ClassLoader.getSystemResource("icons/quiz.jpg")))).setBounds(0, 0, 1440, 392);
 
-        qNoLabel   = new JLabel(); qNoLabel.setBounds(100, 450, 50, 30);
+        add(new JLabel(new ImageIcon(
+             ClassLoader.getSystemResource("icons/quiz.jpg")))).setBounds(0, 0, 1440, 392);
+
+        qNoLabel   = new JLabel(); qNoLabel  .setBounds(100, 450, 50, 30);
         qTextLabel = new JLabel(); qTextLabel.setBounds(150, 450, 900, 30);
-        qNoLabel.setFont  (new Font("Tahoma", Font.PLAIN, 24));
+        qNoLabel  .setFont(new Font("Tahoma", Font.PLAIN, 24));
         qTextLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(qNoLabel); add(qTextLabel);
 
@@ -55,17 +55,16 @@ public class Quiz extends JFrame implements ActionListener {
         opt4 = makeOption(640);
 
         group = new ButtonGroup();
-        group.add(opt1); group.add(opt2);
-        group.add(opt3); group.add(opt4);
+        group.add(opt1); group.add(opt2); group.add(opt3); group.add(opt4);
 
-        /* ----- navigation buttons ----- */
-        nextBtn   = makeNavButton("Next",   550);
-        submitBtn = makeNavButton("Submit", 620);
+        nextBtn   = makeButton("Next",   550);
+        submitBtn = makeButton("Submit", 620);
         submitBtn.setEnabled(false);
 
-        fillQuestions();         
-        pickRandomTen();         
-        display(count);          
+        fillQuestions();
+        pickRandomTen();
+        display(count);
+
         setVisible(true);
     }
 
@@ -78,7 +77,7 @@ public class Quiz extends JFrame implements ActionListener {
         return rb;
     }
 
-    private JButton makeNavButton(String txt, int y) {
+    private JButton makeButton(String txt, int y) {
         JButton b = new JButton(txt);
         b.setBounds(1100, y, 200, 40);
         b.setFont(new Font("Tahoma", Font.PLAIN, 22));
@@ -89,9 +88,109 @@ public class Quiz extends JFrame implements ActionListener {
         return b;
     }
 
+    private void pickRandomTen() {
+        java.util.List<Integer> idx = new java.util.ArrayList<>();
+        for (int i = 0; i < 50; i++) idx.add(i);
+        Collections.shuffle(idx);
+        for (int i = 0; i < 10; i++) selected[i] = idx.get(i);
+    }
+
+    private void display(int num) {
+        stopTickSound();
+        if (swingTimer != null && swingTimer.isRunning()) swingTimer.stop();
+
+        int q = selected[num];
+        qNoLabel.setText((num + 1) + ". ");
+        qTextLabel.setText(questions[q][0]);
+
+        opt1.setText(questions[q][1]); opt1.setActionCommand(questions[q][1]);
+        opt2.setText(questions[q][2]); opt2.setActionCommand(questions[q][2]);
+        opt3.setText(questions[q][3]); opt3.setActionCommand(questions[q][3]);
+        opt4.setText(questions[q][4]); opt4.setActionCommand(questions[q][4]);
+
+        group.clearSelection();
+        startCountdown();
+    }
+
+    private void startCountdown() {
+        timer = 15;
+        timerLabel.setText("Time left - 15 seconds");
+
+        swingTimer = new javax.swing.Timer(1000, e -> {
+            timerLabel.setText("Time left - " + timer + " seconds");
+
+            if (timer == 5) playTickSound();
+            if (timer == 0) playBuzzerSound();
+
+            timer--;
+
+            if (timer < 0) {
+                ((javax.swing.Timer) e.getSource()).stop();
+                stopTickSound();
+                recordAnswer();
+                autoNavigate();
+            }
+        });
+        swingTimer.start();
+    }
+
+    private void playTickSound() {
+        try {
+            stopTickSound();
+            AudioInputStream ais =
+                AudioSystem.getAudioInputStream(getClass().getResource("/icons/tick.wav"));
+            tickClip = AudioSystem.getClip();
+            tickClip.open(ais);
+            tickClip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    private void stopTickSound() {
+        if (tickClip != null && tickClip.isRunning()) {
+            tickClip.stop(); tickClip.close();
+        }
+    }
+
+    private void playBuzzerSound() {
+        try {
+            AudioInputStream ais =
+                AudioSystem.getAudioInputStream(getClass().getResource("/icons/buzzer.wav"));
+            buzzerClip = AudioSystem.getClip();
+            buzzerClip.open(ais);
+            buzzerClip.start();
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    private void recordAnswer() {
+        userAnswers[count][0] =
+            (group.getSelection() == null) ? "" : group.getSelection().getActionCommand();
+    }
+
+    private void autoNavigate() {
+        if (count == 9) submitBtn.doClick();
+        else            nextBtn  .doClick();
+    }
+
+    @Override public void actionPerformed(ActionEvent ae) {
+        if (swingTimer != null) swingTimer.stop();
+        stopTickSound();
+        recordAnswer();
+
+        if (ae.getSource() == nextBtn) {
+            count++;
+            if (count == 9) { nextBtn.setVisible(false); submitBtn.setEnabled(true); }
+            display(count);
+        } else {
+            for (int i = 0; i < 10; i++)
+                if (userAnswers[i][0].equals(answers[selected[i]][1])) score += 10;
+            setVisible(false);
+            new Score(studentName, regNumber, score);
+        }
+    }
+
     private void fillQuestions() {
         String[][] q = {
-             {"What does OOP stand for?", "Object-Oriented Programming", "Only Object Programs", "Open Office Protocol", "Order Of Processing"},
+            {"What does OOP stand for?", "Object-Oriented Programming", "Only Object Programs", "Open Office Protocol", "Order Of Processing"},
             {"What is a class in Java?", "A blueprint for creating objects", "A method", "A function", "A loop"},
             {"What is encapsulation?", "Hiding internal state and requiring all interaction through methods", "Inheritance", "Overloading", "Interface"},
             {"Which OOP concept allows code reuse?", "Inheritance", "Polymorphism", "Abstraction", "Encapsulation"},
@@ -140,84 +239,12 @@ public class Quiz extends JFrame implements ActionListener {
             {"What is object lifecycle?", "Creation to garbage collection", "Compilation only", "Execution only", "Declaration only"},
             {"Can class extend multiple interfaces?", "Yes", "No", "Only abstract", "Only final"},
             {"Is Java 100% OOP?", "No", "Yes", "Always", "Depends"},
-            {"Can interface have static method?", "Yes (since Java 8)", "No", "Only in abstract", "Only private"}
+            {"Can interface have static method?", "Yes (since Java 8)", "No", "Only in abstract", "Only private"},
         };
 
         for (int i = 0; i < q.length; i++) {
             questions[i] = q[i];
-            answers[i][1] = q[i][1];    
-        }
-    }
-
-    private void pickRandomTen() {
-        List<Integer> idx = new ArrayList<>();
-        for (int i = 0; i < 50; i++) idx.add(i);
-        Collections.shuffle(idx);
-        for (int i = 0; i < 10; i++) selected[i] = idx.get(i);
-    }
-
-    private void display(int num) {
-        if (swingTimer != null && swingTimer.isRunning()) swingTimer.stop();
-
-        int qIndex = selected[num];
-        qNoLabel  .setText((num + 1) + ". ");
-        qTextLabel.setText(questions[qIndex][0]);
-
-        opt1.setText(questions[qIndex][1]); opt1.setActionCommand(questions[qIndex][1]);
-        opt2.setText(questions[qIndex][2]); opt2.setActionCommand(questions[qIndex][2]);
-        opt3.setText(questions[qIndex][3]); opt3.setActionCommand(questions[qIndex][3]);
-        opt4.setText(questions[qIndex][4]); opt4.setActionCommand(questions[qIndex][4]);
-
-        group.clearSelection();
-        startCountdown();
-    }
-
-    private void startCountdown() {
-        timer = 15;
-        timerLabel.setText("Time left - 15 seconds");
-
-        swingTimer = new javax.swing.Timer(1000, e -> {
-            timerLabel.setText("Time left - " + timer + " seconds");
-            timer--;
-
-            if (timer < 0) {
-                ((javax.swing.Timer) e.getSource()).stop();
-                recordAnswer();
-                autoNavigate();
-            }
-        });
-        swingTimer.start();
-    }
-
-    private void recordAnswer() {
-        if (group.getSelection() == null)
-            userAnswers[count][0] = "";
-        else
-            userAnswers[count][0] = group.getSelection().getActionCommand();
-    }
-
-    private void autoNavigate() {
-        if (count == 9) { submitBtn.doClick(); }
-        else            { nextBtn  .doClick(); }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        if (swingTimer != null) swingTimer.stop();
-        recordAnswer();
-
-        if (ae.getSource() == nextBtn) {
-            count++;
-            if (count == 9) { nextBtn.setVisible(false); submitBtn.setEnabled(true); }
-            display(count);
-
-        } else if (ae.getSource() == submitBtn) {
-            for (int i = 0; i < 10; i++)
-                if (userAnswers[i][0].equals(answers[selected[i]][1]))
-                    score += 10;
-
-            setVisible(false);
-            new Score(studentName, regNumber, score);
+            answers[i][1] = q[i][1];
         }
     }
 
